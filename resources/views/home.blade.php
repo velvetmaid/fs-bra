a97e69cadb28cfd881bdc00db06939530ef58dd2
Camieux <tarsisiusadhi@gmail.com>
2022-06-19 14:20:47 +0700

    master

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,13 +2,15 @@
 
 @section('container')
 
-<div class="carousel" style="background-image:url(images/home-1.jpg)">
-    <div class="container">
-        <div class="row align-items-center">
-            <div class="main_slider_content">
-                <h6>Spring / Summer Collection 2017</h6>
-                <h1>Get up to 30% Off New Arrivals</h1>
-                <div class="red_button shop_now_button"><a href="#">shop now</a></div>
+<div class="carousel" style="background-image:url(images/home-back.jpg)">
+    <div class="container fill_height">
+        <div class="row align-items-center fill_height">
+            <div class="main_content">
+                <div class="main_slider_content">
+                    <h6>Spring / Summer Collection 2022</h6>
+                    <h1>Get up to 30% Off New Arrivals</h1>
+                    <div class="red_button shop_now_button"><a href="#">shop now</a></div>
+                </div>
             </div>
         </div>
     </div>
