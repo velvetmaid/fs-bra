4537249a1617ca884e7c80a457697eaf0da1c5fb
Camieux <tarsisiusadhi@gmail.com>
2022-06-20 23:06:20 +0700

    master

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -77,4 +77,6 @@
     <script defer src="https://static.cloudflareinsights.com/beacon.min.js/v652eace1692a40cfa3763df669d7439c1639079717194" integrity="sha512-Gi7xpJR8tSkrpF7aordPZQlW2DLtzUlZcumS8dMQjwDHEnw9I7ZLyiOj/6tZStRBGtGgN6ceN6cMH8z7etPGlw==" data-cf-beacon='{"rayId":"71c171828c454c83","token":"cd0b4b3a733644fc843ef0b185f98241","version":"2022.6.0","si":100}' crossorigin="anonymous"></script>
 </body>
 
+@include('partials.footer')
+
 </html>
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,40 @@
+<footer>
+      <div class="main-content">
+        <div class="left box">
+          <h2>About us</h2>
+          <div class="content">
+            <p>CodinNepal is a YouTube channel where you can learn web designing, web development, ui/ux designing, html css tutorial, hover animation and effects, javascript and jquery tutorial and related so on.</p>
+            <div class="social">
+              <a href="https://facebook.com/coding.np"><span class="fab fa-facebook-f"></span></a>
+              <a href="#"><span class="fab fa-twitter"></span></a>
+              <a href="https://instagram.com/coding.np"><span class="fab fa-instagram"></span></a>
+              <a href="https://youtube.com/c/codingnepal"><span class="fab fa-youtube"></span></a>
+            </div>
+          </div>
+        </div>
+        <div class="center box">
+          <h2>Address</h2>
+          <div class="content">
+            <div class="place">
+              <span class="fa-solid fa-location-dot></i>"></span>
+              <span class="text">Cikupa</span>
+            </div>
+            <div class="phone">
+              <span class="fa fa-facebook"></span>
+              <span class="fa fa-facebook"><i class="sr-only">Facebook</i></span>
+              <span class="text">+62-8888-888</span>
+            </div>
+            <div class="email">
+              <span class="fa fa-envelope"></span>
+              <span class="text">abc@example.com</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="bottom">
+        <center>
+          <span class="credit">Created By <a href="https://www.codingnepalweb.com">CodingNepal</a> | </span>
+          <span class="far fa-copyright"></span><span> 2020 All rights reserved.</span>
+        </center>
+      </div>
+    </footer>
