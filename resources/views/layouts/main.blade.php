aa004b46e861bc82920898f00c170273daee0824
Camieux <tarsisiusadhi@gmail.com>
2022-06-22 00:53:42 +0700

    master

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="css/A.style.css">
     <script nonce="2c72c59a-c1e0-4288-95bb-32f4349d7ce2">
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,40 +1,61 @@
 <footer>
       <div class="main-content">
         <div class="left box">
-          <h2>About us</h2>
+          <h2>INI LOGO</h2>
           <div class="content">
-            <p>CodinNepal is a YouTube channel where you can learn web designing, web development, ui/ux designing, html css tutorial, hover animation and effects, javascript and jquery tutorial and related so on.</p>
             <div class="social">
-              <a href="https://facebook.com/coding.np"><span class="fab fa-facebook-f"></span></a>
-              <a href="#"><span class="fab fa-twitter"></span></a>
-              <a href="https://instagram.com/coding.np"><span class="fab fa-instagram"></span></a>
-              <a href="https://youtube.com/c/codingnepal"><span class="fab fa-youtube"></span></a>
+              <a href="/"><img src="images/le-casa-bonita.png" style="width: 15rem;" class="logo-control" /></a>
             </div>
           </div>
         </div>
+
+        <div class="left box">
+          <h2>About us</h2>
+          <div class="content">
+            <p>
+             BELI RUMAH DIMARIH
+            </p>
+            <div class="social">
+              <a href="https://facebook.com"
+                ><span class="fa fa-facebook"></span
+              ></a>
+              <a href="#"><span class="fa fa-twitter"></span></a>
+              <a href="https://instagram.com"
+                ><span class="fa fa-instagram"></span
+              ></a>
+              <a href="https://youtube.com"
+                ><span class="fa fa-youtube"></span
+              ></a>
+            </div>
+          </div>
+        </div>
+
         <div class="center box">
           <h2>Address</h2>
           <div class="content">
             <div class="place">
-              <span class="fa-solid fa-location-dot></i>"></span>
+              <span class="fas fa-map-marker-alt"></span>
               <span class="text">Cikupa</span>
             </div>
             <div class="phone">
-              <span class="fa fa-facebook"></span>
-              <span class="fa fa-facebook"><i class="sr-only">Facebook</i></span>
-              <span class="text">+62-8888-888</span>
+              <span class="fas fa-phone-alt"></span>
+              <span class="text">02108888</span>
             </div>
             <div class="email">
-              <span class="fa fa-envelope"></span>
-              <span class="text">abc@example.com</span>
+              <span class="fas fa-envelope"></span>
+              <span class="text">main@mail.com</span>
             </div>
           </div>
         </div>
       </div>
       <div class="bottom">
         <center>
-          <span class="credit">Created By <a href="#">...</a> | </span>
-          <span class="far fa-copyright"></span><span> 2022 All rights reserved.</span>
+          <span class="credit"
+            >Created By
+            <a href="/">Copyright</a> |
+          </span>
+          <span class="far fa-copyright"></span
+          ><span> 2022 All rights reserved.</span>
         </center>
       </div>
     </footer>
