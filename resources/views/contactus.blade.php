7e07cdc29b366a442def43086f57320f0b79ea2b
Camieux <tarsisiusadhi@gmail.com>
2022-06-23 00:54:10 +0700

    setup f

--- resources/views/contactus.blade.php
+++ resources/views/contactus.blade.php
@@ -8,9 +8,23 @@
     </div>
     <div class="right-content-about">
         <div class="top-left-content-about">
-            <h6>Contact Us</h6>
-            <h1>Judul</h1>
-            <p>lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsumlorem ipsum </p>
+            <h6 class="contact-title">Contact Us</h6>
+            <h4>Easy To Contact Us</h4>
+            <div class="call">
+              <span class="fas fa-phone-alt contact-icon"></span>
+              <span class="icon-name-about">Call</span>
+              <span class="icon-desc-about">021 4444 4444</span>
+            </div>
+            <div class="email">
+              <span class="fas fa-envelope-open-text contact-icon"></span>
+              <span class="icon-name-about">Email</span>
+              <span class="icon-desc-about">Ardito@mail.com</span>
+            </div>
+            <div class="chat">
+              <span class="fas fa-comments contact-icon"></span>
+              <span class="icon-name-about">Chat</span>
+              <span class="icon-desc-about">+62 123 4444</span>
+            </div>
         </div>
         <div class="bottom-left-content-about">
             <span></span>
