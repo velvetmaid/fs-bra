02d72e0eb80aff7ef46692226e4508293918c4bd
Camieux <tarsisiusadhi@gmail.com>
2022-07-30 22:10:25 +0700

    setup layout facilities

--- resources/views/segments/facilities.blade.php
+++ resources/views/segments/facilities.blade.php
@@ -1,23 +1,23 @@
-<section id="page-c" class="d-flex justify-content-center" style="background-color:grey">
+<section id="page-c" class="d-flex justify-content-center">
     <div class="container">
         <div class="container row d-flex justify-content-center">
             <div class="container-fluid col-md-6 col-sm-12 content-facilities-left">
                 <img src="images/facilities-home/item-f.jpg" class="img-fluid" alt="smaple image">
             </div>
             <div class="container-fluid col-md-6 col-sm-12 content-facilities-right">
-                <h1 id="" class="page-title text-center">
+                <h1 id="page-facilities" class="page-title text-center">
                     <span>
                         <span class="h-100 heading-content">Facilites</span>
                     </span>
                 </h1>
-                <div class="row-f row d-flex justify-content-center col-12">
+                <div class="row-item-fac row d-flex justify-content-center col-12">
                     <div class="mb-5 col-6 col-lg-4 p-1 parent-icon-fac">
                         <div class="item-fac">
                             <div class="icon-warp">
                                 <i class="child-icon-fac fa-solid fa-3x fa-person-swimming"></i>
                             </div>
                             <div>
-                                <h3 class="icon-fac-title">Kolam Renang</h3>
+                                <h3 class="icon-fac-title">Swimming Pool</h3>
                             </div>
                         </div>
                     </div>
@@ -37,7 +37,7 @@
                                 <i class="child-icon-fac fa-solid fa-3x fa-mug-hot"></i>
                             </div>
                             <div>
-                                <h3 class="icon-fac-title">Caffe</h3>
+                                <h3 class="icon-fac-title">Mini Caffe</h3>
                             </div>
                         </div>
                     </div>
@@ -67,7 +67,7 @@
                                 <i class="child-icon-fac fa-solid fa-3x fa-person-walking"></i>
                             </div>
                             <div>
-                                <h3 class="icon-fac-title">IDK</h3>
+                                <h3 class="icon-fac-title">Man...</h3>
                             </div>
                         </div>
                     </div>
