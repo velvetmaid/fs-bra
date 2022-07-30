3123bab28d6e884cda5929d6709ca649207750ad
Camieux <tarsisiusadhi@gmail.com>
2022-07-30 23:45:53 +0700

    adding comments

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,5 @@
 <!-- <head>
-
+  
     {{-- Required meta tags --}}
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -14,9 +14,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
   </head> -->
-  
-<!-- Hapus semua comments
-     File Di dalam tag <head></head> taro di file footer.php -->
 
 <footer>
   <div class="container justify-content-center">
