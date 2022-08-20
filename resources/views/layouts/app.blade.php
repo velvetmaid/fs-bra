ce0e4b7177e9ff3c9135defd974c9568d2b85b67
Camieux <tarsisiusadhi@gmail.com>
2022-08-20 12:02:50 +0700

    Remove app.js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,9 +10,6 @@
 
     <title>{{ config('app.name', 'Controls') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
