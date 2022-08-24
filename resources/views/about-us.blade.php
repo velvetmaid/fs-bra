9cb5b1f1826cdd231eecd58f7a5387e3b6bff9cb
Camieux <tarsisiusadhi@gmail.com>
2022-08-24 21:33:49 +0700

    about content

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -23,16 +23,18 @@
 </div>
 <!-- End of Banner -->
 <div class="container">
-    <div class="row">
-        <div class="d-block col-lg-6 col-md-6 col-sm-12">
+    <div class="row text-justify pb-5">
+        <div class="col-lg-6 col-md-6 col-sm-12 d-block">
             <span>About</span>
-            <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel risus ante. Nullam a enim consequat, tristique lacus scelerisque, cursus turpis. Duis pretium sollicitudin nisl, amet rutrum nisi aliquam in.</h1>
+            <div class="">
+                <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel risus ante. Nullam a enim consequat, tristique lacus scelerisque, cursus turpis. Duis pretium sollicitudin nisl, amet rutrum nisi aliquam in.</h1>
+            </div>
         </div>
-        <div class="d-block col-lg-6 col-md-6 col-sm-12">
+        <div class="col-lg-6 col-md-6 col-sm-12 d-block">
             <span>About</span>
-            <div class="ar-center">
-                <p class="pb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel risus ante. Nullam a enim consequat, tristique lacus scelerisque, cursus turpis. Duis pretium sollicitudin nisl, amet rutrum nisi aliquam in.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel risus ante. Nullam a enim consequat, tristique lacus scelerisque, cursus turpis. Duis pretium sollicitudin nisl, amet rutrum nisi aliquam in.</p>
+            <div class="ar-center row">
+                <p class="col-lg-12 col-md-12 col-sm-6" >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel risus ante. Nullam a enim consequat, tristique lacus scelerisque, cursus turpis. Duis pretium sollicitudin nisl, amet rutrum nisi aliquam in.</p>
+                <p class="col-lg-12 col-md-12 col-sm-6" >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel risus ante. Nullam a enim consequat, tristique lacus scelerisque, cursus turpis. Duis pretium sollicitudin nisl, amet rutrum nisi aliquam in.</p>
             </div>
         </div>
     </div>
