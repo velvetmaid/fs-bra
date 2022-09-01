d0a47f0f3d3797ad4eb2d8091a5d301e79a842f8
Camieux <tarsisiusadhi@gmail.com>
2022-09-01 21:06:32 +0700

    indicator carousel

--- resources/views/segments/carousel.blade.php
+++ resources/views/segments/carousel.blade.php
@@ -12,10 +12,10 @@
                 </div>
             </div>
             <ol class="carousel-indicators">
-                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active">1</li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1">2</li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2">3</li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="3">4</li>
             </ol>
             <div class="carousel-inner">
                 <div class="carousel-item active">
@@ -23,26 +23,18 @@
                     </div>
                 </div>
                 <div class="carousel-item">
-                    <div class="overlay-image" style="background-image:url(https://images.unsplash.com/photo-1600585154526-990dced4db0d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHByb3BlcnR5fGVufDB8fDB8fA%3D%3D&w=1000&q=80)" alt="First slide">
+                    <div class="overlay-image" style="background-image:url(https://images.unsplash.com/photo-1600585154526-990dced4db0d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHByb3BlcnR5fGVufDB8fDB8fA%3D%3D&w=1000&q=80)" alt="Second slide">
                     </div>
                 </div>
                 <div class="carousel-item">
-                    <div class="overlay-image" style="background-image:url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTB6kKJHE-aVa2pVFDyKxU_xoQTgW4VtPTGH2ecHtfKWLKvH_ht4H3DMqwWzUcYq-nT5Xg&usqp=CAU)" alt="First slide">
+                    <div class="overlay-image" style="background-image:url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTB6kKJHE-aVa2pVFDyKxU_xoQTgW4VtPTGH2ecHtfKWLKvH_ht4H3DMqwWzUcYq-nT5Xg&usqp=CAU)" alt="Third slide">
                     </div>
                 </div>
                 <div class="carousel-item">
-                    <div class="overlay-image" style="background-image:url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOOI0A5lByk2AqG-5Wh0k7NRRezzsKuEXxeP4HoRI1gz1tqjgmDqPZ4XOMbE6C658_QtM&usqp=CAU)" alt="First slide">
+                    <div class="overlay-image" style="background-image:url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOOI0A5lByk2AqG-5Wh0k7NRRezzsKuEXxeP4HoRI1gz1tqjgmDqPZ4XOMbE6C658_QtM&usqp=CAU)" alt="Four slide">
                     </div>
                 </div>
             </div>
-            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
         </div>
 
     </div>
