900d9738e6cc94f4e61dae37a6175ab19265589e
camieuxgoofy <tarsisiusadhi@gmail.com>
2022-10-15 13:01:56 +0700

    bg-color, welcome position

--- resources/views/segments/carousel.blade.php
+++ resources/views/segments/carousel.blade.php
@@ -1,37 +1,37 @@
-<section id="page-a" class="justify-content-center">
+<section id="page-a">
     <div class="carousel-container">
 
-        <div id="carouselExampleIndicators" class="carousel slide carousel-fade" data-ride="carousel">
-            <div class="wrap-welcome-carousel text-center text-white">
-                <div class="welcome-carousel-inner">
-                    <p class="welcome-carousel-text text-white">LOREM LOREM IPSUM IPSUM LOREM IPSUM</p>
-                    <p class="welcome-carousel-text text-white">LOREM IPSUM LOREM IPSUM</p>
-                    <a href="#content" class="btn btn-welcome tm-btn-cta fa-solid fa-angle-down">
-                        <span>Discover</span>
-                    </a>
+        <div id="carouselExampleIndicators" class="carousel slide carousel-fade pointer-event" data-ride="carousel">
+                <div class="wrap-welcome-carousel text-center text-white">
+                    <div class="welcome-carousel-inner">
+                        <p class="welcome-carousel-text text-white">LOREM LOREM IPSUM IPSUM LOREM IPSUM</p>
+                        <p class="welcome-carousel-text text-white">LOREM IPSUM LOREM IPSUM</p>
+                        <a href="#content" class="btn btn-welcome tm-btn-cta fa-solid fa-angle-down">
+                            <span>Discover</span>
+                        </a>
+                    </div>
                 </div>
-            </div>
-            <ol class="carousel-indicators">
-                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active">1</li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="1">2</li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="2">3</li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="3">4</li>
-            </ol>
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="">1</li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1" class="">2</li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2" class="active">3</li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="3" class="">4</li>
+                </ol>
             <div class="carousel-inner">
+                <div class="carousel-item">
+                    <div class="overlay-image" style="background-image:url(https://images.unsplash.com/photo-1600585154340-be6161a56a0c?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvcGVydHl8ZW58MHx8MHx8&amp;w=1000&amp;q=80)" alt="First slide">
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="overlay-image" style="background-image:url(https://images.unsplash.com/photo-1600585154526-990dced4db0d?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHByb3BlcnR5fGVufDB8fDB8fA%3D%3D&amp;w=1000&amp;q=80)" alt="Second slide">
+                    </div>
+                </div>
                 <div class="carousel-item active">
-                    <div class="overlay-image" style="background-image:url(https://images.unsplash.com/photo-1600585154340-be6161a56a0c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvcGVydHl8ZW58MHx8MHx8&w=1000&q=80)" alt="First slide">
+                    <div class="overlay-image" style="background-image:url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTB6kKJHE-aVa2pVFDyKxU_xoQTgW4VtPTGH2ecHtfKWLKvH_ht4H3DMqwWzUcYq-nT5Xg&amp;usqp=CAU)" alt="Third slide">
                     </div>
                 </div>
                 <div class="carousel-item">
-                    <div class="overlay-image" style="background-image:url(https://images.unsplash.com/photo-1600585154526-990dced4db0d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHByb3BlcnR5fGVufDB8fDB8fA%3D%3D&w=1000&q=80)" alt="Second slide">
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="overlay-image" style="background-image:url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTB6kKJHE-aVa2pVFDyKxU_xoQTgW4VtPTGH2ecHtfKWLKvH_ht4H3DMqwWzUcYq-nT5Xg&usqp=CAU)" alt="Third slide">
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="overlay-image" style="background-image:url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOOI0A5lByk2AqG-5Wh0k7NRRezzsKuEXxeP4HoRI1gz1tqjgmDqPZ4XOMbE6C658_QtM&usqp=CAU)" alt="Four slide">
+                    <div class="overlay-image" style="background-image:url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOOI0A5lByk2AqG-5Wh0k7NRRezzsKuEXxeP4HoRI1gz1tqjgmDqPZ4XOMbE6C658_QtM&amp;usqp=CAU)" alt="Four slide">
                     </div>
                 </div>
             </div>
