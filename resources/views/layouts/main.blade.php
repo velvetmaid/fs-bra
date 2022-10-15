4cbcc833bcbb5233698ba9f2d4ffa35af2d600bd
camieuxgoofy <tarsisiusadhi@gmail.com>
2022-10-15 13:02:50 +0700

    Google Fonts

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -14,6 +14,11 @@
     <link rel="stylesheet" href="https://unpkg.com/aos@2.3.1/dist/aos.css" />
     <link rel="stylesheet" href="css/style.css" />
 
+    {{-- Goolgle Fonts --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;700&family=Qwitcher+Grypen:wght@700&family=Work+Sans&display=swap" rel="stylesheet">
+
     {{-- Font Awesome 6 --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
