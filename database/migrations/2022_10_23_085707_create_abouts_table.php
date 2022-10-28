831ebfee4a16e6639418e69864d672a3ee30908b
camieuxgoofy <tarsisiusadhi@gmail.com>
2022-10-28 08:00:34 +0700

    migration about

---
+++ database/migrations/2022_10_23_085707_create_abouts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('abouts', function (Blueprint $table) {
+            $table->id();
+            $table->text('banner')->nullable();
+            $table->string('banner_text');
+            $table->string('banner_span');
+            $table->string('description');
+            $table->string('visi');
+            $table->string('misi');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('abouts');
+    }
+};
